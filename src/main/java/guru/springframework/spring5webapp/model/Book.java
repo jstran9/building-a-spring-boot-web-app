a37ad0a088a5fe08bc842b5e9735646db1ff72c0
todd <toddtran9@gmail.com>
2018-06-08 16:38:47 -0700

    creating Author POJO.

---
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,84 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    /**
+     * hibernate normally will try to fulfill the relationship of authors and books both ways so it creates
+     * extra tables.
+     * we need to get a single table.
+     * joinColumns=@JoinColumn creates a single id instead of having an author_id and books_id (on the
+     * author_books table) as well as authors_id and book_id (on the book_authors table).
+     */
+    @ManyToMany
+    @JoinTable(name="author_book", joinColumns = @JoinColumn(name="book_id"),
+    inverseJoinColumns = @JoinColumn(name="author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
